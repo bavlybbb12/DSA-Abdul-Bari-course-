911de2d615bebfd2d4d0d23073aaf2f68190d49a
bavlybbb12 <bavlybbb12@gmail.com>
2026-05-05 19:19:44 +0300

    Added two methods of countink linked list nodes

---
+++ 5-Linked list/178-Linked_list_nodes_counter.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std ;
+
+struct Node {
+
+    int data ;
+    struct Node* next ;
+
+};
+
+void create( int arr [] , int n , Node* head) {
+
+    Node* temp , * tail ;
+    
+    head->data = arr [ 0 ] ;
+    head->next = nullptr ;
+    tail = head ;
+    
+    for ( int i = 1 ; i < n ; i++ ) {
+
+        temp = new Node ;
+        temp->data = arr [ i ] ;
+        temp->next = nullptr ;
+        tail->next = temp ;
+        tail = temp ;
+
+    }
+
+}
+
+void display (  Node* head ) {
+
+    while ( head ) {
+        
+        cout << head->data << " " ;
+        head = head->next ;
+
+    }
+}
+
+void display_recursively ( Node* head ) {
+
+    if ( head == nullptr ) return ;
+    cout << head->data << " " ;
+    display_recursively ( head->next ) ;
+}
+
+void destroy ( Node* head ) {
+
+    if ( head == nullptr ) return ;
+    destroy ( head->next ) ;
+    delete head ;
+
+}
+
+void count ( Node* head ) {
+
+    int count = 0;
+    while ( head ) {
+        
+        count++ ;
+        head = head->next ;
+
+    }
+    cout << "Number of nodes: " << count << endl;
+
+}
+
+int count_recursivly ( Node* head ) {
+
+    if ( head ) return 1 + count_recursivly ( head->next ) ;
+    return 0 ;
+
+}
+
+int main () {
+
+    Node* list = new Node ;
+    list->next = nullptr ;
+
+    int arr [] = { 1 , 2 , 3 , 4 , 5 } ;
+
+    create ( arr , sizeof(arr) / sizeof(arr[0]) , list ) ;
+
+    display ( list ) ;
+    cout << endl ;
+
+    display_recursively ( list ) ;
+    cout << endl ;
+
+    count ( list ) ;
+    cout << "Recursive count: " << count_recursivly ( list ) << endl ;
+
+    destroy ( list ) ;
+    list = nullptr ;
+    
+
+}
