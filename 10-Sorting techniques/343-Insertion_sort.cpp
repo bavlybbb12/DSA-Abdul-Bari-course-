f32e204dcf81c7ec5415f6086a0cc9019e0ae8bd
bavlybbb12 <bavlybbb12@gmail.com>
2026-06-13 14:41:16 +0300

    Insertion sort program

---
+++ 10-Sorting techniques/343-Insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void InsertionSort ( vector<int>& arr) {
+
+    if (arr.empty()) return;
+
+    for ( int i = 1; i < arr.size(); i++){
+
+        int key = arr[i];
+        int j = i - 1;
+
+        while ( j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6};
+
+    InsertionSort(arr);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
